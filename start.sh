2602b9debc3e562dcf06f8d20c40795eee4362d0
trofusha <trofusha@trofusha-ThinkBook>
2024-09-19 11:59:02 +0300

    --

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cp .env.example .env
 docker compose down
 docker compose up -d --build --pull always
 
-docker compose exec app php artisan optimize:clear
 docker compose exec app composer install
+docker compose exec app php artisan optimize:clear
 docker compose exec app php artisan migrate:fresh --seed
 docker compose exec app php artisan l5-swagger:generate
